e6e90c432ce2f96e1ac5b8ab18eff327b3620b68
Valentín Avalos <89805271+valentinnavalos@users.noreply.github.com>
2022-04-26 16:52:16 -0300

    feat(Components/LoginForm): add handleOnClick function

--- client/src/Components/LoginForm/LoginForm.jsx
+++ client/src/Components/LoginForm/LoginForm.jsx
@@ -1,24 +1,33 @@
+import CreatorForm from "./CreatorForm/CreatorForm";
+import UserForm from "./UserForm/UserForm";
+
 export default function LoginForm() {
-    const [input, setInput] = useState({
-        name: '',
-        surname: '',
-        // username: '', // Validar que no se repita
-        // mail: '',
-        // password: '',
-        // passwordConfirm: '',
-        // country: '', // Un select que renderice todos los paises?
-        // person: '',//-------> tipo de persona.
-        // role: '',// Cuales van a ser los roles?
-        // idType: '',//-------> tipo de identificacion.
-        // //id file uploaded.
-        // idPhotoFrontside: '',
-        // idPhotoBackside: '',
-        // idNumber: '',
-        // phoneNumber: '',
-        // termsAndConditions: false, 
-    });
+
     const [user, setUser] = useState(false);
     const [creator, setCreator] = useState(false);
+    const [typeOfUser, setTypeOfUser] = useState(false);
+
+    function handleOnClick(e) {
+        // Esta función se ejecuta cuando el cliente decide cómo registrarse:
+        // si decide registrarse como usuario, se cambia el estado de user a true
+        // , el estado de creator a false y se renderiza <UserForm/>
+        // si decide registrarse como creador, se cambia el estado de creator a true
+        // , el estado de user a false y se renderiza <CreatorForm/>
+        if (e.target.name === 'user') {
+            setUser(true);
+            if (creator) {
+                setCreator(false);
+            }
+            setTypeOfUser(true);
+        }
+        if (e.target.name === 'creator') {
+            setCreator(true);
+            if (user) {
+                setUser(false);
+            }
+            setTypeOfUser(true);
+        }
+    }
 
     return (
         <div>
@@ -26,13 +35,13 @@ export default function LoginForm() {
                 <h2>¿Desea registrarse como Usuario o como Creador?</h2>
                 <div >
                     <div >
-                        <button >Usuario</button>
+                        <button name='user' onClick={handleOnClick}>Usuario</button>
                     </div>
                     <div >
-                        <button >Creador</button>
+                        <button name='creator' onClick={handleOnClick}>Creador</button>
                     </div>
                 </div>
-                <form>Formulario</form>
+                {typeOfUser ? user ? <UserForm /> : <CreatorForm /> : <span>Por favor elija como registrarse.</span>}
             </div>
         </div>
     )
