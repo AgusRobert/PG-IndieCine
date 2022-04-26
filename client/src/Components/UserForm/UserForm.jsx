6c4708bcf9492995511c833bfb82d967e12aa20e
Valentín Avalos <89805271+valentinnavalos@users.noreply.github.com>
2022-04-26 16:05:05 -0300

    feat(Components/UserForm): add header skeleton

--- client/src/Components/UserForm/UserForm.jsx
+++ client/src/Components/UserForm/UserForm.jsx
@@ -1,8 +1,18 @@
-export default function UserForm (){
+export default function UserForm() {
     return (
         <div>
-            <div>Header</div>
-            <form>Formulario</form>
+            <div>
+                <h2>¿Desea registrarse como Usuario o como Creador?</h2>
+                <div >
+                    <div >
+                        <button >Usuario</button>
+                    </div>
+                    <div >
+                        <button >Creador</button>
+                    </div>
+                </div>
+                <form>Formulario</form>
+            </div>
         </div>
     )
 }
