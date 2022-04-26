0eefc86ae2e53ede49e83d04c6813b7894351aff
Valentín Avalos <89805271+valentinnavalos@users.noreply.github.com>
2022-04-26 19:40:30 -0300

    feat(Components/LoginForm/CreatorForm): add handleOnSubmit function & add <form> tag
    Agregué la función handleOnSubmit. Y envolví todo el div que retorna con el tag <form></form> colocandole el onSubmit. Además de unos pequeños comentarios para recordar unas cosas.

--- client/src/Components/LoginForm/CreatorForm/CreatorForm.jsx
+++ client/src/Components/LoginForm/CreatorForm/CreatorForm.jsx
@@ -10,13 +10,13 @@ export default function CreatorForm() {
         mail: '',
         password: '',
         passwordConfirm: '',
-        country: '', // Un select que renderice todos los paises?
+        country: '',
         person: '',//-------> tipo de persona.
         role: '',
         idType: '',//-------> tipo de identificacion.
         //id file uploaded.
-        idPhotoFrontside: '',
-        idPhotoBackside: '',
+        idPhotoFrontside: '', // OJO CON ESTO
+        idPhotoBackside: '', // OJO CON ESTO
         idNumber: '',
         phoneNumber: '',
         termsAndConditions: false,
@@ -149,205 +149,234 @@ export default function CreatorForm() {
         });
     }
 
+    function handleOnSubmit(e) {
+        e.preventDefault();
+        setErrors(validateForm(input));
+        if (!Object.keys(errors).length &&
+            input.name &&
+            input.surname &&
+            input.username &&
+            input.mail &&
+            input.password &&
+            input.passwordConfirm &&
+            input.termsAndConditions &&
+            input.country &&
+            input.person &&
+            input.role &&
+            input.idType &&
+            input.idPhotoFrontside && // OJO
+            input.idPhotoBackside && // OJO
+            input.idNumber &&
+            input.phoneNumber
+        ) {
+            alert("Formulario enviado correctamente");
+            // direccionamiento al home?
+        } else {
+            alert("Formulario no enviado");
+        }
+    }
+
     return (
-        <div>
+        <form onSubmit={handleOnSubmit}>
             <div>
                 <div>
-                    <label htmlFor="name">Nombre</label>
-                    <input
-                        type="text"
-                        name="name"
-                        value={input.name}
-                        onChange={handleOnChange}
-                        placeholder="Ingrese su nombre" />
+                    <div>
+                        <label htmlFor="name">Nombre</label>
+                        <input
+                            type="text"
+                            name="name"
+                            value={input.name}
+                            onChange={handleOnChange}
+                            placeholder="Ingrese su nombre" />
+                    </div>
+                    {errors.name && <span>{errors.name}</span>}
                 </div>
-                {errors.name && <span>{errors.name}</span>}
-            </div>
-            <div>
                 <div>
-                    <label htmlFor="surname">Apellido</label>
-                    <input
-                        type="text"
-                        name="surname"
-                        value={input.surname}
-                        onChange={handleOnChange}
-                        placeholder="Ingrese su apellido" />
+                    <div>
+                        <label htmlFor="surname">Apellido</label>
+                        <input
+                            type="text"
+                            name="surname"
+                            value={input.surname}
+                            onChange={handleOnChange}
+                            placeholder="Ingrese su apellido" />
+                    </div>
+                    {errors.surname && <span>{errors.surname}</span>}
                 </div>
-                {errors.surname && <span>{errors.surname}</span>}
-            </div>
-            <div>
                 <div>
-                    <label htmlFor="username">Username</label>
-                    <input
-                        type="text"
-                        name="username"
-                        value={input.username}
-                        onChange={handleOnChange}
-                        placeholder="Ingrese un username" />
+                    <div>
+                        <label htmlFor="username">Username</label>
+                        <input
+                            type="text"
+                            name="username"
+                            value={input.username}
+                            onChange={handleOnChange}
+                            placeholder="Ingrese un username" />
+                    </div>
+                    {errors.username && <span>{errors.username}</span>}
                 </div>
-                {errors.username && <span>{errors.username}</span>}
-            </div>
-            <div>
                 <div>
-                    <label htmlFor="password">Contraseña</label>
-                    <input
-                        type='password'
-                        name="password"
-                        value={input.password}
-                        onChange={handleOnChange}
-                        placeholder="Contraseña" />
+                    <div>
+                        <label htmlFor="password">Contraseña</label>
+                        <input
+                            type='password'
+                            name="password"
+                            value={input.password}
+                            onChange={handleOnChange}
+                            placeholder="Contraseña" />
+                    </div>
+                    {errors.password && <span>{errors.password}</span>}
                 </div>
-                {errors.password && <span>{errors.password}</span>}
-            </div>
-            <div>
                 <div>
-                    <label htmlFor="passwordConfirm">Repita su contraseña</label>
-                    <input
-                        type='password'
-                        name="passwordConfirm"
-                        value={input.passwordConfirm}
-                        onChange={handleOnChange}
-                        placeholder="Contraseña" />
+                    <div>
+                        <label htmlFor="passwordConfirm">Repita su contraseña</label>
+                        <input
+                            type='password'
+                            name="passwordConfirm"
+                            value={input.passwordConfirm}
+                            onChange={handleOnChange}
+                            placeholder="Contraseña" />
+                    </div>
+                    {errors.passwordConfirm && <span>{errors.passwordConfirm}</span>}
                 </div>
-                {errors.passwordConfirm && <span>{errors.passwordConfirm}</span>}
-            </div>
-            <div>
                 <div>
-                    <label htmlFor="mail">Email</label>
-                    <input
-                        type="text"
-                        name="mail"
-                        value={input.mail}
-                        onChange={handleOnChange}
-                        placeholder="Ingrese un mail" />
+                    <div>
+                        <label htmlFor="mail">Email</label>
+                        <input
+                            type="text"
+                            name="mail"
+                            value={input.mail}
+                            onChange={handleOnChange}
+                            placeholder="Ingrese un mail" />
+                    </div>
+                    {errors.mail && <span>{errors.mail}</span>}
                 </div>
-                {errors.mail && <span>{errors.mail}</span>}
-            </div>
-            <div>
                 <div>
-                    <label htmlFor="country">País</label>
-                    <select
-                        name="country"
-                        onChange={handleOnSelect}
-                        defaultValue="">
-                        {countries.length ? (
-                            countries.map(country => (
-                                <option
-                                    key={country.id}
-                                    value={country.id.toLowerCase()}>
-                                    {country.name}
-                                </option>
-                            ))
-                        ) : null}
-                    </select>
+                    <div>
+                        <label htmlFor="country">País</label>
+                        <select
+                            name="country"
+                            onChange={handleOnSelect}
+                            defaultValue="">
+                            {countries.length ? (
+                                countries.map(country => (
+                                    <option
+                                        key={country.id}
+                                        value={country.id.toLowerCase()}>
+                                        {country.name}
+                                    </option>
+                                ))
+                            ) : null}
+                        </select>
+                    </div>
+                    {errors.country && <span>{errors.country}</span>}
                 </div>
-                {errors.country && <span>{errors.country}</span>}
-            </div>
-            <div>
                 <div>
-                    <label htmlFor="person">Persona</label>
-                    <select
-                        name="person"
-                        onChange={handleOnSelect}>
-                        <option value="">Seleccione una opción</option>
-                        <option value="natural">Persona Natural</option>
-                        <option value="juridica">Persona Jurídica</option>
-                    </select>
+                    <div>
+                        <label htmlFor="person">Persona</label>
+                        <select
+                            name="person"
+                            onChange={handleOnSelect}>
+                            <option value="">Seleccione una opción</option>
+                            <option value="natural">Persona Natural</option>
+                            <option value="juridica">Persona Jurídica</option>
+                        </select>
+                    </div>
+                    {errors.person && <span>{errors.person}</span>}
                 </div>
-                {errors.person && <span>{errors.person}</span>}
-            </div>
-            {personSelected ?
+                {personSelected ?
+                    <div>
+                        {
+                            input.person === 'natural' ? (
+                                <select name="role" onChange={handleOnSelect}>
+                                    <option value='' disabled selected>Roles</option>
+                                    <option value='director'>Director/a</option>
+                                    <option value='productor'>Productor/a</option>
+                                    <option value='montajista'>Montajista</option>
+                                </select>
+                            ) : (
+                                <select name="role" onChange={handleOnSelect}>
+                                    <option value='' disabled selected>Roles</option>
+                                    <option value='productora'>Productora</option>
+                                </select>
+                            )
+                        }
+                        {errors.role && <span>{errors.role}</span>}
+                    </div>
+                    : null
+                }
                 <div>
-                    {
-                        input.person === 'natural' ? (
-                            <select name="role" onChange={handleOnSelect}>
-                                <option value='' disabled selected>Roles</option>
-                                <option value='director'>Director/a</option>
-                                <option value='productor'>Productor/a</option>
-                                <option value='montajista'>Montajista</option>
-                            </select>
-                        ) : (
-                            <select name="role" onChange={handleOnSelect}>
-                                <option value='' disabled selected>Roles</option>
-                                <option value='productora'>Productora</option>
-                            </select>
-                        )
-                    }
-                    {errors.role && <span>{errors.role}</span>}
+                    <div>
+                        <label htmlFor="idType">Tipo de identificación</label>
+                        <select
+                            name="idType"
+                            onChange={handleOnSelect}>
+                            <option value="dni">DNI</option>
+                            <option value="pasaporte">Pasaporte</option>
+                            <option value="ruc">RUC</option>
+                        </select>
+                    </div>
+                    {errors.idType && <span>{errors.idType}</span>}
                 </div>
-                : null
-            }
-            <div>
+                <div> {/* OJO */}
+                    <div>
+                        <label htmlFor="idPhotoFrontside">Subir foto del anverso del documento</label>
+                        <input
+                            type="file"
+                            name="idPhotoFrontside"
+                            value={input.idPhotoFrontside}
+                            onChange={handleOnChange}
+                            accept='image/png, image/jpeg, .doc, .docx. .pdf' />
+                    </div>
+                    {errors.idPhotoFrontside && <span>{errors.idPhotoFrontside}</span>}
+                </div>
+                <div> {/* OJO */}
+                    <div>
+                        <label htmlFor="idPhotoBackside">Subir foto del reverso del documento</label>
+                        <input
+                            type="file"
+                            name="idPhotoBackside"
+                            value={input.idPhotoBackside}
+                            onChange={handleOnChange}
+                            accept='image/png, image/jpeg, .doc, .docx. .pdf' />
+                    </div>
+                    {errors.idPhotoBackside && <span>{errors.idPhotoBackside}</span>}
+                </div>
                 <div>
-                    <label htmlFor="idType">Tipo de identificación</label>
-                    <select
-                        name="idType"
-                        onChange={handleOnSelect}>
-                        <option value="dni">DNI</option>
-                        <option value="pasaporte">Pasaporte</option>
-                        <option value="ruc">RUC</option>
-                    </select>
+                    <div>
+                        <label htmlFor="idNumber">Número de identificación</label>
+                        <input
+                            type="text"
+                            name="idNumber"
+                            value={input.idNumber}
+                            onChange={handleOnChange}
+                            placeholder="Ingrese su número de identificación" />
+                    </div>
+                    {errors.idNumber && <span>{errors.idNumber}</span>}
                 </div>
-                {errors.idType && <span>{errors.idType}</span>}
-            </div>
-            <div>
                 <div>
-                    <label htmlFor="idPhotoFrontside">Subir foto del anverso del documento</label>
-                    <input
-                        type="file"
-                        name="idPhotoFrontside"
-                        value={input.idPhotoFrontside}
-                        onChange={handleOnChange}
-                        accept='image/png, image/jpeg, .doc, .docx. .pdf' />
+                    <div>
+                        <label htmlFor="phoneNumber">Teléfono</label>
+                        <input
+                            type="number"
+                            name="phoneNumber"
+                            value={input.phoneNumber}
+                            onChange={handleOnChange} />
+                    </div>
+                    {errors.phoneNumber && <span>{errors.phoneNumber}</span>}
                 </div>
-                {errors.idPhotoFrontside && <span>{errors.idPhotoFrontside}</span>}
-            </div>
-            <div>
                 <div>
-                    <label htmlFor="idPhotoBackside">Subir foto del reverso del documento</label>
-                    <input
-                        type="file"
-                        name="idPhotoBackside"
-                        value={input.idPhotoBackside}
-                        onChange={handleOnChange}
-                        accept='image/png, image/jpeg, .doc, .docx. .pdf' />
+                    <div>
+                        <label htmlFor="mail">Términos y Condiciones</label>
+                        <Link>Link a los términos y condiciones.</Link>
+                        <input
+                            type="checkbox"
+                            name="termsAndConditions"
+                            onChange={handleOnCheckbox} />
+                    </div>
+                    {errors.termsAndConditions && <span>{errors.termsAndConditions}</span>}
                 </div>
-                {errors.idPhotoBackside && <span>{errors.idPhotoBackside}</span>}
             </div>
-            <div>
-                <div>
-                    <label htmlFor="idNumber">Número de identificación</label>
-                    <input
-                        type="text"
-                        name="idNumber"
-                        value={input.idNumber}
-                        onChange={handleOnChange}
-                        placeholder="Ingrese su número de identificación" />
-                </div>
-                {errors.idNumber && <span>{errors.idNumber}</span>}
-            </div>
-            <div>
-                <div>
-                    <label htmlFor="phoneNumber">Teléfono</label>
-                    <input
-                        type="number"
-                        name="phoneNumber"
-                        value={input.phoneNumber}
-                        onChange={handleOnChange} />
-                </div>
-                {errors.phoneNumber && <span>{errors.phoneNumber}</span>}
-            </div>
-            <div>
-                <div>
-                    <label htmlFor="mail">Términos y Condiciones</label>
-                    <Link>Link a los términos y condiciones.</Link>
-                    <input
-                        type="checkbox"
-                        name="termsAndConditions"
-                        onChange={handleOnCheckbox} />
-                </div>
-                {errors.termsAndConditions && <span>{errors.termsAndConditions}</span>}
-            </div>
-        </div>
+        </form>
     )
 }
