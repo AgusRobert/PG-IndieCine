75e0af2689268f4b867173e3f71e329a23deda98
Valentín Avalos <89805271+valentinnavalos@users.noreply.github.com>
2022-04-27 16:26:29 -0300

    feat(redux\reducer): pisa exactamente a como estan hechos los cambios

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -1,5 +1,5 @@
 import {SEARCH_PELIS , FILTER_DURATION , ORDER_DATE } from "../actions/actionstype";
-
+import {DATE_DES} from './Ordercosntants'
 const initialState = {
   peliculas: [],
   pelisfiltradas: [],
@@ -25,10 +25,10 @@ function rootReducer(state = initialState, action) {
       let orderMoviesDate = [...state.pelisfiltradas];
       orderMoviesDate = orderMoviesDate.sort((a, b) => {
         if (a.date < b.date) {
-          return action.payload === POB_DES ? -1 : 1;
+          return action.payload === DATE_DES ? -1 : 1;
         }
         if (a.date > b.date) {
-          return action.payload === POB_DES ? 1 : -1;
+          return action.payload === DATE_DES ? 1 : -1;
         }
         return 0;
       });
