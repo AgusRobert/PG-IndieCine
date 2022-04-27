48ddba012729e773083cdc90ebc424ad21b9a458
Agustín IABICHELLA ROBERT <iabichellarobert@gmail.com>
2022-04-27 15:21:47 -0300

    Agregado del ordenamiento por fecha
    Agrego la carpeta de constants asi podemos compilar el resto de las variables de ordenamiento y ademas puse la logica de ordenamiento en el reducer.

---
+++ client/src/redux/reducer/Ordercosntants.js
@@ -0,0 +1,2 @@
+export const DATE_ASC = 'DATE_ASC'
+export const DATE_DES = 'DATE_DES'
--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -1,4 +1,4 @@
-import { SEARCH_PELIS, FILTER_DURATION } from "../actions/actionstype";
+import {SEARCH_PELIS , FILTER_DURATION , ORDER_DATE } from "../actions/actionstype";
 
 const initialState = {
   peliculas: [],
@@ -21,7 +21,21 @@ function rootReducer(state = initialState, action) {
         ...state,
         pelisfiltradas: action.payload,
       };
-
+    case ORDER_DATE:
+      let orderMoviesDate = [...state.pelisfiltradas];
+      orderMoviesDate = orderMoviesDate.sort((a, b) => {
+        if (a.date < b.date) {
+          return action.payload === POB_DES ? -1 : 1;
+        }
+        if (a.date > b.date) {
+          return action.payload === POB_DES ? 1 : -1;
+        }
+        return 0;
+      });
+      return {
+        ...state,
+        pelisfiltradas: orderMoviesDate,
+      };
     default:
       return "hola";
   }
