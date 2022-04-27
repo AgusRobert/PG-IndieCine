c0719dad7ff403266c0c34a3278af17012f4a180
Valentín Avalos <89805271+valentinnavalos@users.noreply.github.com>
2022-04-27 14:41:33 -0300

    feat:(Component\LogInBtn): some changes

--- client/src/Components/LogInBtn/LogInBtn.jsx
+++ client/src/Components/LogInBtn/LogInBtn.jsx
@@ -1,7 +1,19 @@
+import { useDispatch } from "react-redux";
+import { logInLogOut } from "../../redux/actions";
+
 export default function LogInBtn() {
+
+    const dispatch = useDispatch();
+    const { isLogedIn } = useSelector(state => state);
+
+    function handleOnClick(e) {
+        e.preventDefault();
+        dispatch(logInLogOut());
+    }
+
     return (
         <div>
-            <button>Log In</button>
+            <button onClick={handleOnClick}>{isLogedIn ? ('Log out') : ('Log in')}</button>
         </div>
     )
 }
