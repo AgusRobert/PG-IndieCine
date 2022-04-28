2a8f1c8ab17097adfdf651e9f851fdbd05058e09
Valentín Avalos <89805271+valentinnavalos@users.noreply.github.com>
2022-04-28 14:28:58 -0300

    feat(Components\SignInBtn): add logout btn

--- client/src/Components/SignInBtn/SignInBtn.jsx
+++ client/src/Components/SignInBtn/SignInBtn.jsx
@@ -1,3 +1,4 @@
+import { useAuth0 } from '@auth0/auth0-react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { signInSignOut } from '../../redux/actions';
@@ -8,10 +9,14 @@ export default function SignInBtn() {
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
+    // ----- Auth0 logout -----
+    const { logout } = useAuth0();
+
     function handleOnSign(e) {
         e.preventDefault();
         if (isSignIn) {
             //despachar la accion para desloguear
+            logout({ returnTo: window.location.origin });
             dispatch(signInSignOut(false));
         } else {
             //redirecciona al formulario
