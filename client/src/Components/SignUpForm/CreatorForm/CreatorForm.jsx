f8a7bf925199d40fa0cc5b9fb475dde4adf32003
Valentín Avalos <89805271+valentinnavalos@users.noreply.github.com>
2022-04-28 19:20:54 -0300

    feat(Components\CreatorForm): some code fixes

--- client/src/Components/SignUpForm/CreatorForm/CreatorForm.jsx
+++ client/src/Components/SignUpForm/CreatorForm/CreatorForm.jsx
@@ -115,6 +115,11 @@ export default function CreatorForm() {
             input.phoneNumber
         ) {
             alert("Formulario enviado correctamente");
+
+            // despachar la accion para updatear el usuario.
+            // Si la ruta post es la misma que el usuario básico,
+            // agregar el booleano 'creator' en true.
+
             // direccionamiento al home? al login?
             // resetear el estado de input??
         } else {
@@ -125,78 +130,6 @@ export default function CreatorForm() {
     return (
         <form onSubmit={handleOnSubmit}>
             <div>
-                {/* <div>
-                    <div>
-                        <label htmlFor="name">Nombre</label>
-                        <input
-                            type="text"
-                            name="name"
-                            value={input.name}
-                            onChange={handleOnChange}
-                            placeholder="Ingrese su nombre" />
-                    </div>
-                    {errors.name && <span>{errors.name}</span>}
-                </div> */}
-                {/* <div>
-                    <div>
-                        <label htmlFor="surname">Apellido</label>
-                        <input
-                            type="text"
-                            name="surname"
-                            value={input.surname}
-                            onChange={handleOnChange}
-                            placeholder="Ingrese su apellido" />
-                    </div>
-                    {errors.surname && <span>{errors.surname}</span>}
-                </div> */}
-                {/* <div>
-                    <div>
-                        <label htmlFor="username">Username</label>
-                        <input
-                            type="text"
-                            name="username"
-                            value={input.username}
-                            onChange={handleOnChange}
-                            placeholder="Ingrese un username" />
-                    </div>
-                    {errors.username && <span>{errors.username}</span>}
-                </div> */}
-                {/* <div>
-                    <div>
-                        <label htmlFor="password">Contraseña</label>
-                        <input
-                            type='password'
-                            name="password"
-                            value={input.password}
-                            onChange={handleOnChange}
-                            placeholder="Contraseña" />
-                    </div>
-                    {errors.password && <span>{errors.password}</span>}
-                </div> */}
-                {/* <div>
-                    <div>
-                        <label htmlFor="passwordConfirm">Repita su contraseña</label>
-                        <input
-                            type='password'
-                            name="passwordConfirm"
-                            value={input.passwordConfirm}
-                            onChange={handleOnChange}
-                            placeholder="Contraseña" />
-                    </div>
-                    {errors.passwordConfirm && <span>{errors.passwordConfirm}</span>}
-                </div> */}
-                {/* <div>
-                    <div>
-                        <label htmlFor="mail">Email</label>
-                        <input
-                            type="text"
-                            name="mail"
-                            value={input.mail}
-                            onChange={handleOnChange}
-                            placeholder="Ingrese un mail" />
-                    </div>
-                    {errors.mail && <span>{errors.mail}</span>}
-                </div> */}
                 <div>
                     <div>
                         <label htmlFor="country">País</label>
