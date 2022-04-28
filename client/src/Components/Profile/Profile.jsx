55deaeaae84b4d9d573ac29ddddb8112d79ac8db
Valentín Avalos <89805271+valentinnavalos@users.noreply.github.com>
2022-04-28 18:42:39 -0300

    feat(Components\Profile): add <CreateForm> component

--- client/src/Components/Profile/Profile.jsx
+++ client/src/Components/Profile/Profile.jsx
@@ -1,37 +1,43 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import CreatorForm from "../SignUpForm/CreatorForm/CreatorForm";
 
 export default function Profile() {
 
-    const [isCreatorClick, setIsCreatorClick] = useState(false);
+    // const [isCreatorClick, setIsCreatorClick] = useState(false);
 
-    function handleOnClick() {
-        setIsCreatorClick(!isCreatorClick);
-    }
+    // function handleOnClick() {
+    //     setIsCreatorClick(!isCreatorClick);
+    // }
 
     return (
         <div>
+            <div>
+                {/* div provisorio */}
+                <Link to={'/'}>Home (link provisorio)</Link>
+            </div>
             <div>
                 <h1>Profile</h1>
             </div>
             <div>
                 <h2>Mis datos</h2>
+                <span>Todos los datos del usuario</span>
             </div>
             <div>
                 <h2>¿Desea subir al siguiente nivel?</h2>
-                <button onClick={handleOnClick}>Subir a Creador</button>
-                {isCreatorClick ? <CreatorForm /> : (
-                    <div>
-                        <h4>Beneficios de convertirse en Creador.</h4>
-                        <ul>
-                            <li>Posibilidad de publicar tu contenido.</li>
-                            <li>Visibilidad a tu contenido.</li>
-                            <li>Sección donde gestionar tu contenido.</li>
-                        </ul>
-                    </div>
-
-                )}
-
+                <div>
+                    <h4>Beneficios de convertirse en Creador.</h4>
+                    <ul>
+                        <li>Posibilidad de publicar tu contenido.</li>
+                        <li>Visibilidad a tu contenido.</li>
+                        <li>Sección donde gestionar tu contenido.</li>
+                    </ul>
+                </div>
+            </div> <br />
+            {/* <button onClick={handleOnClick}>Subir a Creador</button> */}
+            <div>
+                <h4>Para cambiar a Creador, por favor complete el siguiente formulario.</h4>
+                <CreatorForm />
             </div>
         </div>
     )
