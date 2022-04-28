afcf886dc820043cf13b0bdefecdd59c0bb4dc17
Valentín Avalos <89805271+valentinnavalos@users.noreply.github.com>
2022-04-28 18:17:35 -0300

    feat(Components\Profile): add CreatorForm option
    Además de la posibilidad (futura) de modificar sus datos, agrego el btn para renderizar el formulario para convertir su perfil a 'Creado'

--- client/src/Components/Profile/Profile.jsx
+++ client/src/Components/Profile/Profile.jsx
@@ -1,5 +1,37 @@
+import CreatorForm from "../SignUpForm/CreatorForm/CreatorForm";
+
 export default function Profile() {
+
+    const [isCreatorClick, setIsCreatorClick] = useState(false);
+
+    function handleOnClick() {
+        setIsCreatorClick(!isCreatorClick);
+    }
+
     return (
-        <div>Mi Perfil</div>
+        <div>
+            <div>
+                <h1>Profile</h1>
+            </div>
+            <div>
+                <h2>Mis datos</h2>
+            </div>
+            <div>
+                <h2>¿Desea subir al siguiente nivel?</h2>
+                <button onClick={handleOnClick}>Subir a Creador</button>
+                {isCreatorClick ? <CreatorForm /> : (
+                    <div>
+                        <h4>Beneficios de convertirse en Creador.</h4>
+                        <li>
+                            <ul>Posibilidad de publicar tu contenido.</ul>
+                            <ul>Visibilidad a tu contenido.</ul>
+                            <ul>Sección donde gestionar tu contenido.</ul>
+                        </li>
+                    </div>
+
+                )}
+
+            </div>
+        </div>
     )
 }
