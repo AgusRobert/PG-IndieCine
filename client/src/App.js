9d0c2c4c7d81280c81c4adfc5a856d8e2248a254
Valentín Avalos <89805271+valentinnavalos@users.noreply.github.com>
2022-04-28 17:30:23 -0300

    feat: add signUp with auth0

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Home from "./Components/Home/Home";
 import About from "./Components/About/About";
 import Contact from "./Components/Contact/Contact";
 import TyC from "./Components/T&C/T&C";
-import SignUpForm from "./Components/SignUpForm/SignUpForm";
+// import SignUpForm from "./Components/SignUpForm/SignUpForm";
 // import SignInForm from "./Components/SignInForm/SignInForm";
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           {/* <Route exact path="/signin" element={<SignInForm />} /> */}
-          <Route exact path="/signup" element={<SignUpForm />} />
+          {/* <Route exact path="/signup" element={<SignUpForm />} /> */}
           <Route exact path="/about" element={<About />} />
           <Route exact path="/contact" element={<Contact />} />
           <Route exact path="/terms" element={<TyC />} />
