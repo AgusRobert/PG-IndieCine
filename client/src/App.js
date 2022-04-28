2b45a1b60158fbcecab279b2ecbb89657e388e28
Valentín Avalos <89805271+valentinnavalos@users.noreply.github.com>
2022-04-28 18:25:36 -0300

    feat(App.js): add <Profile> route

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Home from "./Components/Home/Home";
 import About from "./Components/About/About";
 import Contact from "./Components/Contact/Contact";
 import TyC from "./Components/T&C/T&C";
+import Profile from "./Components/Profile/Profile";
 // import SignUpForm from "./Components/SignUpForm/SignUpForm";
 // import SignInForm from "./Components/SignInForm/SignInForm";
 
@@ -15,6 +16,7 @@ function App() {
           <Route exact path="/" element={<Home />} />
           {/* <Route exact path="/signin" element={<SignInForm />} /> */}
           {/* <Route exact path="/signup" element={<SignUpForm />} /> */}
+          <Route exact path="/profile" element={<Profile />} />
           <Route exact path="/about" element={<About />} />
           <Route exact path="/contact" element={<Contact />} />
           <Route exact path="/terms" element={<TyC />} />
