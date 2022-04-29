2d6649c549b5b14ae502ac0f7f54632a79976e6a
keyn4 <keynarafaelp@gmail.com>
2022-04-29 10:09:31 -0500

    :id  para la ruta de getById
     1 solo cambio

--- api/src/routes/film.js
+++ api/src/routes/film.js
@@ -4,7 +4,7 @@ const router = Router();
 const {getFilms, postFilms, updateFilm, getById} = require('../controllers/film')
 
 router.get('/', getFilms)
-router.get('/', getById)
+router.get('/:id', getById)
 router.post('/', postFilms)
 router.put('/', updateFilm)
 
