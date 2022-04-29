9dc98d5f1b3d3f16e0aa19772a4d89f180236e66
guenga1968 <gustavogallesio@hotmail.com>
2022-04-28 22:16:07 -0300

    subida de imagenes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,8 +3,14 @@ const router = Router();
 
 //Aqui tus Router -- No subir este archivo Plis! :)
 
+const apiFilmsRouter = require("./film");
+const apiUsersRouter = require("./user");
+const apiComentsRouter = require("./comment");
+const apiUploadRouter = require("./upload");
 
-
-
+router.use("/films", apiFilmsRouter);
+router.use("/users", apiUsersRouter);
+router.use("/comments", apiComentsRouter);
+router.use("/upload", apiUploadRouter);
 
 module.exports = router;
