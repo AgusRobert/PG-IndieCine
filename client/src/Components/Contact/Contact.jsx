41c4368c5543da9e260b2375776c1c316fa9a829
Valentín Avalos <89805271+valentinnavalos@users.noreply.github.com>
2022-04-30 19:34:39 -0300

    feat: more divs

--- client/src/Components/Contact/Contact.jsx
+++ client/src/Components/Contact/Contact.jsx
@@ -3,10 +3,16 @@ import React from "react";
 export default function Contact() {
     return (
         <div>
-            <h2>Contacto</h2>
-            <h4>Ante cualquier inconveniente por favor comuniquese con el soporte técnico, que se encuentra trabajando 24/7</h4>
-            <span>Teléfono: 0810-122-3568</span>
-            <span>Email: soporteindiecine@gmail.com </span>
+            <div>
+                <h2>Contacto</h2>
+            </div>
+            <div>
+                <h4>Ante cualquier inconveniente por favor comuniquese con el soporte técnico, que se encuentra trabajando 24/7</h4>
+                <div>
+                    <span>Teléfono: 0810-122-3568</span>
+                    <span>Email: soporteindiecine@gmail.com </span>
+                </div>
+            </div>
         </div>
     )
 }
