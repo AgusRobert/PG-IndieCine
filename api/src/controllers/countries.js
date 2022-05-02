8799bbb8523df53fd959ca35e49812ac3b7a32cd
Valentín Avalos <89805271+valentinnavalos@users.noreply.github.com>
2022-05-02 19:31:10 -0300

    fix: fix a bugg

--- api/src/controllers/countries.js
+++ api/src/controllers/countries.js
@@ -1,37 +1,43 @@
-'use strict';
-const { Country } = require('../db.js');
+"use strict";
+const { Country } = require("../db.js");
 
 //POST countries
 const postCountries = async (req, res, next) => {
-    try {
-        const { name } = req.body;
-        const country = await Country.create({
-            name
-        });
-        res.json({
-            message: 'Country created',
-            country
-        });
-    } catch (error) {
-        next(error);
+  try {
+    const { name } = req.body;
+    const [country, created] = await Country.findOrCreate({
+      where: { name },
+    });
+    if (created) {
+      res.json({
+        message: "Country created",
+        country,
+      });
+    } else {
+      res.json({
+        message: "Country already exists",
+        country,
+      });
     }
+  } catch (error) {
+    next(error);
+  }
 };
 
 //GET all countries
 const getCountries = async (req, res, next) => {
-    try{
-        const allCountries = await Country.findAll({
-            order: [['name', 'ASC']] //para que en el llamado del select aparezcan ordenados de A a Z, aunque ya estan guardados en la tabla en orden alfabético
-        });
+  try {
+    const allCountries = await Country.findAll({
+      order: [["name", "ASC"]], //para que en el llamado del select aparezcan ordenados de A a Z, aunque ya estan guardados en la tabla en orden alfabético
+    });
 
-        res.status(200).json(allCountries);
-
-    } catch(error){
-        next(error);
-    }
+    res.status(200).json(allCountries);
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
-    getCountries,
-    postCountries
+  getCountries,
+  postCountries,
 };
--- api/src/controllers/genres.js
+++ api/src/controllers/genres.js
@@ -1,37 +1,43 @@
-'use strict';
-const { Genre } = require('../db.js');
+"use strict";
+const { Genre } = require("../db.js");
 
 //POST genres
 const postGenres = async (req, res, next) => {
-    try {
-        const { name } = req.body;
-        const genre = await Genre.create({
-            name
-        });
-        res.json({
-            message: 'Genre created',
-            genre
-        });
-    } catch (error) {
-        next(error);
+  try {
+    const { name } = req.body;
+    const [genre, created] = await Genre.findOrCreate({
+      where: { name: name },
+    });
+    if (created) {
+      res.json({
+        message: "Genre created",
+        genre,
+      });
+    } else {
+      res.json({
+        message: "Genre already exists",
+        genre,
+      });
     }
+  } catch (error) {
+    next(error);
+  }
 };
 
 //GET all genres
 const getGenres = async (req, res, next) => {
-    try{
-        const allGenres = await Genre.findAll({
-            order: [['name', 'ASC']]
-        });
+  try {
+    const allGenres = await Genre.findAll({
+      order: [["name", "ASC"]],
+    });
 
-        res.status(200).json(allGenres);
-
-    } catch(error){
-        next(error);
-    }
+    res.status(200).json(allGenres);
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
-    getGenres,
-    postGenres
-}
+  getGenres,
+  postGenres,
+};
