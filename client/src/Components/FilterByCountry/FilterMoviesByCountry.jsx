129e0b3b32020d5830c387d62e37496d8cffb061
Agustín IABICHELLA ROBERT <iabichellarobert@gmail.com>
2022-05-01 23:31:16 -0300

    correccion de merge

--- client/src/Components/FilterByCountry/FilterMoviesByCountry.jsx
+++ client/src/Components/FilterByCountry/FilterMoviesByCountry.jsx
@@ -17,42 +17,10 @@ const SelectStyle = styled(TextField)({
   padding: 0,
 });
 
-import { styled } from '@mui/material/styles';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import InputBase from '@mui/material/InputBase';
-
-const BootstrapInput = styled(InputBase)(({ theme }) => ({
-  'label + &': {
-    marginTop: theme.spacing(1),
-  },
-  '& .MuiInputBase-input': {
-    color: "#D892FD",
-    position: 'relative',
-    backgroundColor: theme.palette.background.paper,
-    fontSize: 16,
-    padding: '10px 26px 10px 12px',
-    transition: theme.transitions.create(['border-color']),
-    // Use the system font instead of the default Roboto font.
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
-  },
-}));
 
 export default function FilterMovieByCountry(){
 
+   let key = 1
     const dispatch = useDispatch();
 
     const countries = useSelector((state) => state.countries);
