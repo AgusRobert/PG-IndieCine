45a4524c427bd58f8cfab4c496745220eee4c9f1
gastond90 <94383358+gastond90@users.noreply.github.com>
2022-05-01 23:10:02 -0300

    otros

--- client/src/Components/About/About.jsx
+++ client/src/Components/About/About.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Footer from "../Footer/Footer.jsx"
-export default function About(){ //los parametros son los que quiero que aparezcan en la card
+export default function About(){
     return (
        <>
        <h1>Sobre nosotros</h1>
+       <p style={{fontSize: '22px'}}>Somos un grupo de compañeros de estudio de distintos puntos de Latinoamérica que decidimos desarrollar esta Web como proyecto final de nuestra carrera de Full Stack-Developers.
+       <p> CINDIE nace de una necesidad que sentimos por una plataforma que haga posible el acceso a material audiovisual independiente latinoamericano. El enfoque de la página está en el archivado y almacenado de proyectos de productoras o individuos pertenecientes a las artes audiovisuales y en brindarle a los usuarios la oportunidad de hacer donaciones a los artistas.</p>
+        <p>  Con CINDIE nos proponemos ofrecer un amplio catálogo de largometrajes de ficción, documentales y cortometrajes realizados por artistas latinoamericanos. Nuestro objetivo a futuro es que nuestro catálogo contenga una amplia variedad de películas de todos los géneros y nacionalidades, tanto de autores consagrados como de nuevos talentos del cine independiente. </p>
+        </p>
        <Footer/>
        </> 
     )
