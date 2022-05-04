eda970a3edf5d74f440dbdc2256a145d8ade2640
fsr1410 <faq.ramos.14101999@gmail.com>
2022-05-04 12:11:17 -0300

    agregado de la relacion cirular entre comentarios

--- api/src/models/Comment.js
+++ api/src/models/Comment.js
@@ -1,33 +1,40 @@
 const { Model, DataTypes } = require("sequelize");
 
-class Comment extends Model{};
+class Comment extends Model {}
 
 module.exports = (sequelize) => {
-    return Comment.init({
-        id:{
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        body:{
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        type:{
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        status:{
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        numberReport:{
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-    },{
-        sequelize,
-        tableName:"comment",
-        timestamps: true,
-    });
+  return Comment.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      body: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      type: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      status: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      numberReport: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      parentId: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+    },
+    {
+      sequelize,
+      tableName: "comment",
+      timestamps: true,
+    }
+  );
 };
