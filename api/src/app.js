d8d319d9573c0f82daac2e1554cb7a3be862f42e
guenga1968 <gustavogallesio@hotmail.com>
2022-05-08 22:28:14 -0300

    mail desde admin

--- api/src/app.js
+++ api/src/app.js
@@ -1,23 +1,18 @@
-const { urlencoded } = require("express");
-require("./db.js");
-const app = require("express");
-
-const AdminJS = require('adminjs');
-const AdminJSExpress = require('@adminjs/express');
-const AdminJSSequelize = require('@adminjs/sequelize');
+const AdminJS = require("adminjs");
+const AdminJSExpress = require("@adminjs/express");
+const AdminJSSequelize = require("@adminjs/sequelize");
 AdminJS.registerAdapter(AdminJSSequelize);
 
 const express = require("express");
-const morgan = require('morgan');
-const routes = require('./routes/index.js');
-require("./db.js");
+const morgan = require("morgan");
+const routes = require("./routes/index.js");
+const path = require("path");
 
-const path = require('path');
 const server = express();
 
-server.set('views', path.join(__dirname, 'views'));
-server.set('view engine', 'ejs');
-server.use(express.static(path.join(__dirname, 'public')));
+server.set("views", path.join(__dirname, "views"));
+server.set("view engine", "ejs");
+server.use(express.static(path.join(__dirname, "public")));
 
 server.name = "PG-IndieCine";
 server.use(express.json());
@@ -25,30 +20,54 @@ server.use(express.urlencoded({ extended: true }));
 server.use(morgan("dev"));
 
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
 });
 
-const con = require('./db.js').connect;
-
-const adminJs = new AdminJS({
-    databases: [con],
+const { User, Film, Country, Genre, Comment, Plans } = require("./db.js");
+const adminJsOptions = {
     rootPath: '/admin',
-  })
-  const router = AdminJSExpress.buildRouter(adminJs)
-  server.use(adminJs.options.rootPath, router)
-  
+   database:[],
+   resources: [
+       {resource: User, options: {
+        actions: {enviarMail:{
+            actionType: 'record',
+            icon:'Email',
+            component: AdminJS.bundle('./myMail.jsx'),
+            handler:async(req, res, context) =>{
+                return {record: context.record.toJSON(),}},}},
+        parent: {icon:'User'}} },
+        {resource: Film, options: {parent: {icon:'Camera'}} },
+        {resource: Country, options: { parent: {icon:'Map'}} },
+    {resource: Genre, options: {parent: {icon:'Star'}} },
+         {resource: Comment, options: {parent: {icon:'Chat'}} },
+         {resource: Plans, options: {parent: {icon:'Product'}} },
+],
+   branding: {
+       logo: './LOGO.png',
+       companyName: 'C I N D I E',
+   }
+}
+
+
+const adminJs = new AdminJS(adminJsOptions);
+
+const router = AdminJSExpress.buildRouter(adminJs);
+server.use(adminJs.options.rootPath, router);
 
 server.use("/", routes);
 
 server.use((err, req, res, next) => {
-    const status = err.status || 500;
-    const message = err.message || err;
-    console.error(err);
-    res.status(status).send(message);
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
 });
-  
+
 module.exports = server;
