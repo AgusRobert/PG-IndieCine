935c19515496185db1439047edc6c3a956fa5838
guenga1968 <gustavogallesio@hotmail.com>
2022-05-18 12:52:30 -0300

    se cambio localhost por ruta back

--- client/src/paths/path.js
+++ client/src/paths/path.js
@@ -1,2 +1,2 @@
-export const SERVER_BACK = "http://localhost:3001";
-export const SERVER_FRONT = "http://localhost:3000";
+export const SERVER_BACK = "https://cinde.herokuapp.com";
+export const SERVER_FRONT = " https://cindie-front.vercel.app";
